926c898c7eca62d553300c353de00de4c77abeef
sumitsaurabh3 <sumitsaurabh799@gmail.com>
2024-03-14 09:55:46 +0530

    final commit

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "RESTORANT",
+      
+    })
+    .then(() => {
+      console.log("Connected to database!");
+    })
+    .catch((err) => {
+      console.log(`Some error occured while connecing to database: ${err}`);
+    });
+};
